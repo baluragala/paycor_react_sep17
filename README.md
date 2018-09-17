6b0e936c25a2dba5ea3498227943c367dcdecf07
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-17 19:13:10 +0530

    scoping

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # paycor_react_sep17
+
+## Overview
+
+24hrs of programme, focused on hands on
+4 hr per day , 50 mins class
+handson breaks
+
+## Tools
+
+vs code
+PPT
+
+github - https://github.com/baluragala/paycor_react_sep17
+
+collabedit -> http://collabedit.com/jude9
---
+++ js/scoping.js
@@ -0,0 +1,15 @@
+function loop() {
+  // for (var i = 0; i < 10; i++) {
+  //   console.log(i); //i is visible thus is logged in the console as 0,1,2,....,9
+  // }
+
+  for (let i = 0; i < 10; i++) {
+    console.log(i); //i is visible thus is logged in the console as 0,1,2,....,9
+  }
+  // console.log("out side loop", i);
+}
+
+loop();
+
+const PI = 3.414;
+PI = 40;
