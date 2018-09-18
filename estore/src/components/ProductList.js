3ccc7b6453c7c00d4c8f90b26eeb65239ff03580
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-18 22:29:39 +0530

    proptypes

--- estore/src/components/ProductList.js
+++ estore/src/components/ProductList.js
@@ -5,6 +5,7 @@ class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      message: "",
       products: [
         {
           id: 1,
@@ -14,7 +15,7 @@ class ProductList extends Component {
         {
           id: 2,
           title: "pixel xl",
-          price: 1200
+          price: "1200"
         },
         {
           id: 3,
@@ -24,7 +25,7 @@ class ProductList extends Component {
         {
           id: 4,
           title: "MI note",
-          price: 600
+          price: "600"
         },
         {
           title: "pixel 3",
@@ -38,15 +39,24 @@ class ProductList extends Component {
       this.state.products.push({ id: 6, title: "iphone xs", price: 1999 });
       this.setState({ products: this.state.products });
     }, 5000);
+
+    this.onProductClicked = this.onProductClicked.bind(this);
+  }
+
+  onProductClicked(title) {
+    this.setState({ message: `${title} was clicked` });
   }
 
   render() {
     return (
       <div>
-        <h1>Products</h1>
+        <h1>Products ({this.state.message})</h1>
         <ul>
           {this.state.products.map(p => (
-            <ProductListItem product={p} />
+            <ProductListItem
+              product={p}
+              productClicked={this.onProductClicked}
+            />
           ))}
         </ul>
       </div>
--- estore/src/components/ProductListItem.js
+++ estore/src/components/ProductListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProductListItem.css";
+import PropTypes from "prop-types";
 
 // function ProductListItem(props) {
 //   let {
@@ -16,15 +17,24 @@ import "./ProductListItem.css";
 class ProductListItem extends React.Component {
   render() {
     let {
-      product: { title, price }
+      product: { title, price },
+      productClicked
     } = this.props;
     return (
-      <div className="item">
+      <div className="item" onClick={() => productClicked(title)}>
         <h2>{title}</h2>
         <h3>{price}</h3>
       </div>
     );
   }
 }
+
+ProductListItem.propTypes = {
+  product: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+  }).isRequired,
+  productClicked: PropTypes.func.isRequired
+};
 
 export default ProductListItem;
