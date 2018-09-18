f01598fa5ac70195733c1ce900e793d53ba33cde
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-18 21:34:53 +0530

    state

--- estore/src/App.js
+++ estore/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <ProductList />
         <Footer />
       </div>
     );
---
+++ estore/src/components/ProductList.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          id: 1,
+          title: "iphone x",
+          price: 2000
+        },
+        {
+          id: 2,
+          title: "pixel xl",
+          price: 1200
+        },
+        {
+          id: 3,
+          title: "note 7",
+          price: 1000
+        },
+        {
+          id: 4,
+          title: "MI note",
+          price: 600
+        },
+        {
+          title: "pixel 3",
+          price: 1000,
+          id: 5
+        }
+      ]
+    };
+
+    setTimeout(() => {
+      this.state.products.push({ id: 6, title: "iphone xs" });
+      this.setState({ products: this.state.products });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Products</h1>
+        <ul>
+          {this.state.products.map(p => (
+            <li>{p.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
