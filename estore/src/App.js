4ef2354a0b91d559b932e09e9f1d7c1357e0c417
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-18 21:13:47 +0530

    components

--- estore/src/App.js
+++ estore/src/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
-    let message = "Hello React";
-    let fruits = ["Apple", "Banana", "Chikoo"];
     return (
       <div>
-        {/* this 
-      is 
-    only 
-  commment syntax is JSX*/}
-        <p
-          title="this is jsx expression"
-          className="info high"
-          style={{
-            backgroundColor: "orange",
-            fontSize: 30
-          }}
-          myCustomAttribute="1"
-        >
-          {message}
-        </p>
-        <p>{message.toUpperCase()}</p>
-        <p>{new Date().toDateString()}</p>
-        <p>{2 + 2 == 4 ? "Yes" : "No"}</p>
-        <ul>
-          {fruits.map(f => (
-            <li>{f}</li>
-          ))}
-        </ul>
-        <p>{[1, 2, 3, 4, 5, 6]}</p>
+        <Header />
+        <Footer />
       </div>
     );
   }
