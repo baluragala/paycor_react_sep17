fecff93ad314fdb29e6bd9cfecf39036d3ef0037
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-19 22:12:53 +0530

    routing - dnoe

--- estore/src/App.js
+++ estore/src/App.js
@@ -5,17 +5,30 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import NoMatch from "./components/NoMatch";
 import ProductDetail from "./components/ProductDetail";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: true };
+  }
   _renderRoutes() {
     return (
       <Switch>
         <Route exact path="/products" component={ProductList} />
-        <Route path="/products/new" component={AddProduct} />
+        <Route
+          path="/products/new"
+          render={props =>
+            this.state.isLoggedIn ? (
+              <AddProduct {...props} myProp="1" />
+            ) : (
+              <Redirect to="/products" />
+            )
+          }
+        />
         <Route path="/products/:pid" component={ProductDetail} />
         <Route component={NoMatch} />
       </Switch>
